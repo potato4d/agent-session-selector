4685fc77002cb466a479acd9ffd8d1fcf9ab2ad3
Takuma HANATANI <mail@potato4d.me>
2026-03-30 00:11:12 +0900

    Restore default macOS app menu

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,7 @@
-#[cfg(target_os = "macos")]
-use tauri::menu::Menu;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .setup(|app| {
-      #[cfg(target_os = "macos")]
-      {
-        let menu = Menu::default(app.handle())?;
-        menu.set_as_app_menu()?;
-      }
-
       if cfg!(debug_assertions) {
         app.handle().plugin(
           tauri_plugin_log::Builder::default()
